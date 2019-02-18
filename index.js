53ef2e7abc4b417191e925a7d68d5ff63d877097
divineadriano <adrianodivine@gmail.com>
2019-02-18 19:31:00 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+
+var app = express();
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', function(req, res) {
+	res.render('pages/kyc');
+});
+
+//kyc
+app.get('/kyc', function(req, res) {
+	res.render('pages/kyc');
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
